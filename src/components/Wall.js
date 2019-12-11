bc5476bd2fe73abf7e3bca1995f5cff2cec64269
Nolan Picini <nolanpic@gmail.com>
2019-12-11 12:16:56 -0800

    Fetching APOD using useEffect

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,2 +1,23 @@
 
-// will hold all state
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { api_key } from '../config';
+
+const Wall = () => {
+
+    const [apod, setApod] = useState(null);
+    const [date, setDate] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
+            .then(res => setApod(res.data));
+    }, [date]);
+
+    return (
+        <div>
+            {apod && <img src={apod.url} />}
+        </div>
+    );
+};
+
+export default Wall;
